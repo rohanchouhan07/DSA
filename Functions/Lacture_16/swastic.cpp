34e2e4e071a8fe652d44974a602a63fea2c75627
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-02-06 14:12:59 +0530

    rohan

--- Functions/Lacture_16/swastic.cpp
+++ Functions/Lacture_16/swastic.cpp
@@ -15,8 +15,14 @@ int main(){
     cin>>n;
     int m=(n+1)/2;
     for(int i=1;i<=n;i++){
+        // first row
         if(i==1){
-            
+            {
+                cout<<"* ";
+                for(int j=1;j<n-1;j++){
+                    cout<<"* ";
+                }
+            }
         }
     }
 }
