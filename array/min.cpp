f22a26b21a2a4c5326fbb620c039b254254a26e2
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-02-09 14:40:58 +0530

    rohan

--- array/min.cpp
+++ array/min.cpp
@@ -2,16 +2,17 @@
 // #include<climits>
 using namespace std;
 int main(){
-    int max=0;
+    int min=0;//min=INT_MIN;
     int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+    
     for(int i=0;i<n;i++){
-        if(max<=arr[i])            
-        max=arr[i];
+        if(min>=arr[i])            
+        min=arr[i];
         }
-        cout<<"Maximun is :"<<max;
+        cout<<"Minimun is ::"<<min;
 }
