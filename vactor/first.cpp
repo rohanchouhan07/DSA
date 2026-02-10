d1fade03ec796fda89cff88c19bfc88eb974c86b
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-02-10 15:46:34 +0530

    rohan

--- vactor/first.cpp
+++ vactor/first.cpp
@@ -11,7 +11,8 @@ int main(){
     a.push_back(5);
     a.push_back(6);
     a.push_back(7);
-
+    cout<<a.size()<<endl;
+    cout<<a.capacity()<<endl;
     for(int i=0;i<a.size();i++){
         cout<<a[i]<<" ";
     }
