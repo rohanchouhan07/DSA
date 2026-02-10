d23d45c6a28b148e8fe9869d6173b47f59e32af3
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-02-10 16:08:19 +0530

    rohan

--- vactor/first.cpp
+++ vactor/first.cpp
@@ -4,17 +4,36 @@ using namespace std;
 
 int main(){
     vector<int>  a;
-    a.push_back(1);
-    a.push_back(2);
-    a.push_back(3);
-    a.push_back(4);
-    a.push_back(5);
-    a.push_back(6);
-    a.push_back(7);
+    // a.push_back(1);
+    // a.push_back(2);
+    // a.push_back(3);
+    // a.push_back(4);
+    // a.push_back(5);
+    // a.push_back(6);
+    // a.push_back(7);
+    // a.push_back(7);
+    // a.push_back(7);
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        a.push_back(i);
+    }
+    // size
     cout<<a.size()<<endl;
-    cout<<a.capacity()<<endl;
+    // capacity
+    cout<<a.capacity()<<endl<<"normal array \t";
     for(int i=0;i<a.size();i++){
-        cout<<a[i]<<" ";
+            cout<<a[i]<<" ";
+    }
+    cout<<endl<<"delete last element \t";
+    a.pop_back();
+    for(int i=0;i<a.size();i++){
+            cout<<a[i]<<" ";
+    }
+    cout<<endl<<"add element \t";
+    a.insert(a.begin()+2,(5,5));
+    for(int i=0;i<a.size();i++){
+            cout<<a[i]<<" ";
     }
     return 0;
 }
