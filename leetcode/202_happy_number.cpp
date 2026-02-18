1dabc9a6bd10a0ade083ac5ea454aa7d0938934a
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-02-18 13:51:38 +0530

    rohan

--- leetcode/202_happy_number.cpp
+++ leetcode/202_happy_number.cpp
@@ -2,23 +2,26 @@
 #include<cmath>
 using namespace std;
 
-int square(int n){
-    return n*n;
-}
+bool isHappy(int n) {
+        int  sum=0;
+        if(n==1 or n==7 or n==10){
+        return true;
+    }
+    else if(n<10){
+        return false;
+    }else{
+    while(n!=0){
+        int k= n%10;
+        sum=sum+k*k;
+        n/=10;
+        }
+
+        return isHappy(sum);
+    }
+    }
 int main(){
     int n;
     cin>>n;
-    int arr[]={};
-    while(n!=0){
-        int k= n%10;
-        cin>>arr[k];
-        if(pow(arr[1],2)+pow(arr[2],2)==1){
-            cout<<"True";
-        }  
-        else{
-            cout<<"False";
-        } 
-
-    }
-    return 0;
+    isHappy(n);
+    // return 0;
         }
