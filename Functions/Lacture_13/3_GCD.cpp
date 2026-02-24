53d675fbbed7b4ea1f4514ebe9b18a60f3673419
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-02-24 15:40:57 +0530

    rohan

--- Functions/Lacture_13/3_GCD.cpp
+++ Functions/Lacture_13/3_GCD.cpp
@@ -11,5 +11,5 @@ int main()
             break;
         }
     }
-    return 1;
+    return 0;
 }
---
+++ leetcode/Que4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> merge(vector<int> a,vector<int> b){
+    int i=0,j=0;
+    int m=a.size();
+    int n=b.size();
+    vector<int> nums;
+    while (a<m and j<n0)
+    {
+        if(a[i]>b[j]);
+    }
+    
+}
