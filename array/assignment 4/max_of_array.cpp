210821f4b745aae11654fe9e30224dc687a17b42
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-03 13:08:27 +0530

    rohan

---
+++ array/assignment 4/max_of_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,max=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ;i<n;i++){
+        cin>>arr[i];
+        int max=arr[0];
+    }
+    for(int i=0 ;i<n;i++){
+        if(max < arr[i]){
+            max=arr[i];
+        }
+    }
+    cout<<max;
+    return 0;
+}
--- array/assignment 4/square_Of_array.cpp
+++ array/assignment 4/square_Of_array.cpp
@@ -10,13 +10,15 @@ int main(){
         cin>>arr[i];
         square[i]=arr[i]*arr[i];
     }
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<=n-1;j++){
-    //         if(square[j]>square[j+1]){
-    //             swap(square[j],square[j+1]);
-    //         }
-    //     }
-    // }
+    for(int i=0;i<n;i++){
+
+        for(int j=0;j<n-1;j++){
+
+            if(square[j] > square[j+1]){
+                swap(square[j],square[j+1]);
+            }
+        }
+    }
     for(int i=0;i<n;i++){
         cout<<square[i]<<" ";
     }
