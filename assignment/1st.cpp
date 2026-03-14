550d31195cc1cf84ca4cefcdab4633edead0119a
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-14 11:15:14 +0530

    rohan

---
+++ assignment/1st.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    <vector <vector<int>>v(n,<vector><int>m) ;
+    
+    return 0;
+}
---
+++ string/palindrome.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int x=123;
+    int rev;
+    reverse
+    cout<<rev;
+    return 0;
+}
