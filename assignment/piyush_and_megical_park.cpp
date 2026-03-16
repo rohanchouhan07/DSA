b04c7a4ad201d9dbe1632b9a59f276e467461fbb
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-17 01:27:23 +0530

    r

---
+++ assignment/piyush_and_megical_park.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m,k,s;
+    cin>>n>>m;
+    cin>>k>>s;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr[i][j] == '.' ){
+                s-=3;
+            }
+            else if(arr[i][j] == '*'){
+                s+=5;
+            }
+            else if(arr[i][j]=='#'){
+                continue;
+            }
+        }
+    }
+    if(s<=k){
+        cout<<"No";
+    }
+    else{
+        cout<<"Yes";
+    }
+
+    return 0;
+}
