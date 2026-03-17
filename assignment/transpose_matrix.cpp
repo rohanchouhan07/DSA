250972f0fd9b9292b2d3e6a801b19acbcee9445b
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-17 08:15:28 +0530

    rohan

---
+++ assignment/transpose_matrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    // cout<<endl;
+    for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            cout<<arr[i][j]<<" ";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
