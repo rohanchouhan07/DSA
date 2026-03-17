c6d228e3dd42410e54ba7aa41042cb55364feb35
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-17 11:55:05 +0530

    rohan

---
+++ assignment/square_root.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+int squareRoot(int A) {
+        int n;
+        n=sqrt(A);
+        return n;
+
+}
+int main () {
+        int A;
+        cin >> A;
+        cout << squareRoot(A);
+	return 0;
+}
