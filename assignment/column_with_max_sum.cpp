42e144596fdbe5a6b70311ec0fa9769ef0e59735
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-18 00:16:26 +0530

    r

--- assignment/column_with_max_sum.cpp
+++ assignment/column_with_max_sum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    int sum=0;
+    int sum=0,s=sum;
     int arr[n][n];
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -13,11 +13,15 @@ int main() {
     }
 
     for(int i=0;i<n;i++){
+        int p=i;
         for(int j=0;j<n;j++){
             sum+=arr[i][j];
+            cout<<"max sum is:",sum;
         }
-        
+    //     // if(s<=sum){
+    //     //     s=sum;
+    //     // }
     }
-    cout<<"max sum is:",sum;
+    cout<<sum;
     return 0;
 }
