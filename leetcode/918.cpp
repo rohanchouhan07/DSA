30f299e376083bfd2f5b91af1eae694c7be6d7a5
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-18 13:30:38 +0530

    r

---
+++ leetcode/918.cpp
@@ -0,0 +1,13 @@
+// ! circluar array
+#include <iostream>
+using namespace std;
+
+int main() {
+    nums
+    int maxsum=nums[0];
+    int minsum=nums[0];
+    int currentsum=nums[0];
+    int currentmaxsum=nums[0];
+    int currentminsum=nums[0];
+    return 0;
+}
