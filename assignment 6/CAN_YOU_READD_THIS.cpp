c312a4ce02823335522548621a821325baff9c02
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-19 21:48:50 +0530

    rohan

--- assignment 6/CAN_YOU_READD_THIS.cpp
+++ assignment 6/CAN_YOU_READD_THIS.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include<string>
+#include<cctype>
 using namespace std;
 
 int main() {
     string s;
     cin>>s;
+    int count=0;
     for(int i=0;i<s.size();i++){
-        
+        // if(isupper(s[i])){
+            cout<<isupper(s[i])+islower(s[i])<<endl;
+        // }
     }
     return 0;
 }
